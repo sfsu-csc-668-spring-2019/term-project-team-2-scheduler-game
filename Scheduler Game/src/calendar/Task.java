f706c835571aaea61c9021d7cdd6e00c95d00fa8
mattvil <mattvilain@gmail.com>
2019-04-28 13:07:41 -0700

    Project creation using AIEngine and begin of task list initialisation

--- Scheduler Game/src/calendar/Task.java
+++ Scheduler Game/src/calendar/Task.java
@@ -1,4 +1,57 @@
 package calendar;
 
+import scheduler.Scheduler;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.UUID;
+
 public class Task {
+
+    private String id;
+    private Duration duration;
+    private LocalDateTime begin;
+    private LocalDateTime end;
+    private int status;         //0:todoo, 1:in-progress, 2:Done, 3:Not-finish
+    private String description;
+    private HashMap<String, Boolean> todoList;
+    private float productivity;
+    private float satisfaction;
+
+    private Scheduler observer;
+
+    public Task(LocalDateTime begin, Duration duration) {
+        this.id = UUID.randomUUID().toString();
+        this.begin = begin;
+        this.duration = duration;
+        this.end = this.begin.plusMinutes(this.duration.toMinutes());
+        this.status = 0;
+        this.productivity = (float) 0.0;
+        this.satisfaction = (float) 0.0;
+    }
+
+    public void addDescription(String description) {
+        this.description = description;
+    }
+
+    public void addTodoList(HashMap<String, Boolean> todoList) {
+        this.todoList = todoList;
+    }
+
+    public void attach(Scheduler scheduler) {
+        this.observer = scheduler;
+    }
+
+    public void notifyObserver() {
+        this.observer.taskUpdate(this);
+    }
+
+    public String toString(){
+        String str = "";
+        str += "id: " + this.id + "\n";
+        str += "Begin: " + this.begin + "\tEnd: " + this.end +  "\tDuration: " + this.duration + "\n";
+        str += "Status: " + this.status + "\n";
+        return str;
+    }
 }

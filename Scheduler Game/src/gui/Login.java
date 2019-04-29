46289320dda1c7dac0e0e031d19a64014ef3067a
Vinicius Miazaki <v.miazaki@gmail.com>
2019-04-28 21:41:07 -0700

    Login updated

--- Scheduler Game/src/gui/Login.java
+++ Scheduler Game/src/gui/Login.java
@@ -1,69 +1,76 @@
 package gui;
 
+import java.io.File;
+import java.io.IOException;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import javax.imageio.ImageIO;
 
 public class Login extends JFrame implements ActionListener {
 
-    JPanel panel;
-    JLabel user_label, password_label, message;
-    JTextField userName_text;
-    JPasswordField password_text;
-    JButton submit;
+    JPanel containerPanel, loginPanel;
+    JLabel lbUsername, lbPassword, lbMessage;
+    JTextField txtUsername;
+    JPasswordField txtPassword;
+    JButton btnEnter;
 
     public Login() {
 
         // User Label
-        user_label = new JLabel();
-        user_label.setText("User Name :");
-        userName_text = new JTextField();
+        lbUsername = new JLabel();
+        lbUsername.setText("Username :");
+        txtUsername = new JTextField();
 
         // Password
+        lbPassword = new JLabel();
+        lbPassword.setText("Password :");
+        txtPassword = new JPasswordField();
 
-        password_label = new JLabel();
-        password_label.setText("Password :");
-        password_text = new JPasswordField();
+        lbMessage = new JLabel();
+        btnEnter = new JButton("SUBMIT");
+        btnEnter.addActionListener(this);
 
-        // Submit
+        loginPanel = new JPanel(new GridLayout(3, 1));
+        loginPanel.setMaximumSize(new Dimension(300, 100));
+        loginPanel.add(lbUsername);
+        loginPanel.add(txtUsername);
+        loginPanel.add(lbPassword);
+        loginPanel.add(txtPassword);
+        loginPanel.add(lbMessage);
+        loginPanel.add(btnEnter);
 
-        submit = new JButton("SUBMIT");
+        containerPanel = new JPanel();
+        containerPanel.setLayout(new BoxLayout(containerPanel, BoxLayout.Y_AXIS));
+        containerPanel.setBorder(BorderFactory.createEmptyBorder(80, 0, 0, 0));
+        containerPanel.add(loginPanel);
 
-        panel = new JPanel(new GridLayout(3, 1));
-
-        panel.add(user_label);
-        panel.add(userName_text);
-        panel.add(password_label);
-        panel.add(password_text);
-
-        message = new JLabel();
-        panel.add(message);
-        panel.add(submit);
-
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        // Adding the listeners to components..
-        submit.addActionListener(this);
-        add(panel, BorderLayout.CENTER);
-        setTitle("Please Login Here !");
-        setSize(900, 600);
+        // JFrame settings
+        try {this.setContentPane(new JLabel(new ImageIcon(ImageIO.read(new File("test.jpg"))))); }
+        catch (IOException e) {e.printStackTrace();}
+        this.pack();
+        this.setVisible(true);
+        
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.add(containerPanel, BorderLayout.CENTER);
+        this.setTitle("Scheduler Login");
+        this.setSize(400, 300);
         this.setResizable(false);
         this.setLocationRelativeTo(null);
-        setVisible(true);
-
+        this.setVisible(true);
     }
 
     @Override
     public void actionPerformed(ActionEvent ae) {
-        String userName = userName_text.getText();
-        String password = password_text.getText();
+        String userName = txtUsername.getText();
+        String password = String.valueOf(txtPassword.getPassword());
 
         if (userName.trim().equals("admin") && password.trim().equals("admin")) {
             new MainGUI();
+            this.dispose();
         } else {
-            message.setText(" Invalid user.. ");
+            lbMessage.setText(" Invalid user.. ");
         }
-
     }
 }
